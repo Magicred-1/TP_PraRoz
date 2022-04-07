c224ca4c34744a1250c5507c929af08510275cdb
Djason Gadiou <86433099+Magicred-1@users.noreply.github.com>
2022-04-07 14:23:33 +0200

    Implementing responsive CSS

--- js/app.js
+++ js/app.js
@@ -1,41 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log(navigator.userAgent);
+
+    let regexMail, regexMdp, warning, email, password, btn;
     
-    let regexmail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
-    let regexmdp = /^[a-zA-Z0-9._-]{2,15}$/;
+    regexMail = /^[a-zA-Z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+    regexMdp = /^[a-zA-Z0-9._-]{2,15}$/;
+    warning = document.querySelector(".warning");
+    email = document.getElementById("email").value;
+    password = document.getElementById("password").value;
+    btn = document.getElementById("connexion");
 
-    const user = {
-        warning: document.querySelector(".form p"),
-        email: document.getElementsByTagName("input")[3].value,
-        password: document.getElementsByTagName("input")[3].value,
-        btn:document.getElementsByTagName("input")[4]
-    };
-
-    user.btn.addEventListener("click", e => {
+    btn.addEventListener("click", e => {
         e.preventDefault();
-        formActive();
+        formConnexion();
     });
     
-    let formActive = () => {
+    const formConnexion = () => {
 
-        if (user.email.match(regexmail) && user.password.match(regexmdp)) {
-            user.warning.classList.add("success");
-            user.warning.classList.remove("error");
-            user.warning.innerText = "Connexion réussie";
+        if (email.match(regexMail) && password.match(regexMdp)) {
+            warning.classList.add("success");
+            warning.classList.remove("error");
+            warning.innerText = "Connexion réussie";
+            console.log(warning);
             localStorage.setItem("user", email);
             sessionStorage.setItem("user", "sessionid");
-        } else if (user.password.match(regexmdp)) {
-            user.warning.classList.add("error");
-            user.warning.innerText = "Mail invalide";
-            user.warning.classList.remove("success");
-        } else if (user.email.match(regexmail)) {
-            user.warning.classList.add("error");
-            user.warning.innerText = "Mot de passe invalide";
-            user.warning.classList.remove("success");
-        } else {
-            user.warning.classList.add("error");
-            user.warning.innerText = "Mail ou mot de passe incorrecte";
-            user.warning.classList.remove("success");
+        } else if (!password.match(regexMail)) {
+            warning.classList.add("error");
+            warning.innerText = "Mail invalide";
+            console.log(warning);
+            warning.classList.remove("success");
         };
     };
 });
